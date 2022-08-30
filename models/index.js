046597f218f02c447c9ec41e9b1f79fc807e1843
samvrny <samvrny@gmail.com>
2022-08-30 13:47:05 -0500

    built some associations

--- models/index.js
+++ models/index.js
@@ -20,10 +20,40 @@ Bill.belongsTo(Group, {
     foreignKey: 'group_id'
 });
 
-// Bill.belongsToMany(User {
-//     through: Group,
-//     as: 'split_bill',
-//     foreignKey: 
-// })
+Chore.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Chore, {
+    foreignKey: 'user_id'
+});
+
+//For the grocerys, the Grocery(item) itself could be associated with individual users,
+//for now I have it written to just be associated with a group, as in a group can have a list
+//with many grocery that can be interacted with. This can be changed so that a grocery also
+//belongs to a single user, or another association can be added.
+Grocery.belongsTo(Group, {
+    foreignKey: 'group_id'
+});
+
+Group.hasMany(Grocery, {
+    foreignKey: 'group_id'
+});
+
+//these many to many associations are probably going to need some help. I anticipate there
+//needing to be some kind of change with the foreign keys. Once the server is running
+//and some basic routes are created I will run more testing and get the accociations nailed
+//down.
+Bill.belongsToMany(User, {
+    through: Group,
+    as: 'split_bill',
+    foreignKey: 'group_id'
+});
+
+User.belongsToMany(Bill, {
+    through: Group,
+    as: 'split-bill',
+    foreignKey: 'user_id'
+});
 
 module.exports = { User, Bill, Group, Chore, Grocery };

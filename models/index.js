f7f6fa61073a52e8ac1384130e788101fd9d193d
samvrny <105232555+samvrny@users.noreply.github.com>
2022-08-30 11:41:16 -0500

    Feature/sam (#10)
    * added my name to the README
    
    * created User model
    
    * build Bill model
    
    * build Group model
    
    Co-authored-by: AlexCourtney18 <102307529+AlexCourtney18@users.noreply.github.com>

--- models/index.js
+++ models/index.js
@@ -1,12 +1,27 @@
 const User = require('./User');
 const Bill = require('./Bill');
+const Group = require('./Group');
 
-User.hasMany(Bill, {
-    foreignKey: 'user_id'
+Group.hasMany(User, {
+    foreignKey: 'group_id'
 });
 
-Bill.belongsTo(User, {
-    foreignKey: 'user_id'
+User.belongsTo(Group, {
+    foreignKey: 'group_id'
 });
 
-module.exports = { User, Bill };
+Group.hasMany(Bill, {
+    foreignKey: 'group_id'
+});
+
+Bill.belongsTo(Group, {
+    foreignKey: 'group_id'
+});
+
+// Bill.belongsToMany(User {
+//     through: Group,
+//     as: 'split_bill',
+//     foreignKey: 
+// })
+
+module.exports = { User, Bill, Group };

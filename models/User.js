bca5c0c2ea62df5eeb507704482531b1e38aa090
kelsie kidd <kelsiekidd93@gmail.com>
2022-08-31 12:44:01 -0500

    accepting incoming changes

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-//const sequelize = require('../config/connection');
-const sequelize = require('../test-connection');
+const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt') //Will need later to hash user passwords.
 
 

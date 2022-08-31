223e5427ecdb9af31e77ca73c969a98319818a90
samvrny <samvrny@gmail.com>
2022-08-31 11:45:00 -0500

    fix

--- models/Bill.js
+++ models/Bill.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-//const sequelize = require('../config/connection');
-const sequelize = require('../test-connection');
+const sequelize = require('../config/connection');
 
 class Bill extends Model {}
 

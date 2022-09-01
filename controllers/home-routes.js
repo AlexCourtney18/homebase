b21e67a3496012ab4f43e1acd879fec84711132c
Olivia <ramsfol@gmail.com>
2022-08-31 19:22:24 -0500

    fixing merge

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,10 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Bill, Chore, Grocery, Group, User } = require('../models');
 
+router.get('/', (req, res) => {
+    res.render('homepage')
+})
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})

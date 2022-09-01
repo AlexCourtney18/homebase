e549df42fd3ed4070cb864f8a9e3559a74cf2d75
kidd93 <104868408+kidd93@users.noreply.github.com>
2022-09-01 12:49:58 -0500

    Feature/kelsie (#34)
    * feature branch
    
    * Connected to database
    
    * added routes to server.js
    
    * accepting incoming changes
    
    * auth and logout files
    
    Co-authored-by: AlexCourtney18 <102307529+AlexCourtney18@users.noreply.github.com>

---
+++ public/javascript/logout.js
@@ -0,0 +1,14 @@
+async function logout() {
+    const answer = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: {'content-type' : 'application/json'}
+    });
+
+    if (answer.ok) {
+        document.location.replace('/');
+    } else {
+        alert(answer.statusText);
+    }
+}
+
+document.querySelector('#').addEventListener('click', logout);

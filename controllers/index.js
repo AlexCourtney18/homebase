7eb20b1c13de32369587b37081cf6954c8128510
Alex <lex.courtney18@gmail.com>
2022-09-01 10:41:29 -0500

    create landing page and login route

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const landingRoutes = require('./landing-routes');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+router.use('/', landingRoutes);
+router.use('/homepage', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;

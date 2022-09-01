3fe9a701f82cf207a520bc0502789eedcf411e47
AlexCourtney18 <102307529+AlexCourtney18@users.noreply.github.com>
2022-09-01 09:59:37 -0500

    Feature/alex (#29)
    * create user-routes
    
    * add code to bill-routes
    
    * add group-routes
    
    * add chore routes and modify bill routes
    
    * commit before pulling
    
    * finish grocery routes
    
    * remove test code
    
    * commit before pulling
    
    * commit change to gitignore before pulling
    
    * move main.handlebars code into login
    
    * add dashboard and homepage routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,68 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Bill, Chore, Grocery, Group, User } = require('../models');
+const { Bill, Chore, Grocery, Group } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    res.render('homepage')
-})
+router.get('/', withAuth, (req, res) => {
+    Group.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'group_name',
+            'address'
+        ]
+    })
+        .then(dbGroupData => {
+            const groups = dbGroupData.map(group => group.get({ plain: true }));
+            res.render('homepage', { groups, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
-})
+router.get('/dashboard/:id', withAuth, (req, res) => {
+    Group.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'group_name',
+            'address',
+            'user_id'
+        ],
+        include: [
+            {
+                model: Bill,
+                attributes: ['id', 'company', 'amount_due', 'due_date', 'status', 'group_id']
+            },
+            {
+                model: Chore,
+                attributes: ['id', 'chore_name']
+            },
+            {
+                model: Grocery,
+                attributes: ['id', 'grocery_name']
+            }
+        ]
+    })
+    .then(dbGroupData => {
+        const group = dbGroupData.get({ plain: true });
+
+        res.render('dashboard', {
+            group,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;

4b45229461ac079fe12fdc6e79aea36224ce5e55
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-09-04 11:44:21 -0500

    finished creategroup.js

--- controllers/api/group-routes.js
+++ controllers/api/group-routes.js
@@ -57,7 +57,7 @@ router.post('/', (req, res) => {
         Group.create({
             group_name: req.body.group_name,
             address: req.body.address,
-            user_id: req.body.user_id
+            user_id: req.session.user_id
         })
         .then(dbGroupData => res.json(dbGroupData))
         .catch(err => {

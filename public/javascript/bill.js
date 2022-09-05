d1aa39466bc8565d0426bc413e350383fdc5e3d7
OliviaRamsfield <105067386+OliviaRamsfield@users.noreply.github.com>
2022-09-05 11:32:38 -0500

    Feature/ramsfield (#47)
    * added to project
    
    * created feature branch
    
    * fixing merge conflict
    
    * fixing merge
    
    * reorganizing handlebars files
    
    * added js files for homepage functionality
    
    * moving code to correct page
    
    * homepage HTML/starting spin wheel
    
    * added nav bars and started bill.js
    
    * updated dashboard layout/forms/nav
    
    * updated handlebar layouts/buttons
    
    * started custom styling
    
    * styled homepage and updated custom styles
    
    * wheel functionality and bill js updates
    
    Co-authored-by: AlexCourtney18 <102307529+AlexCourtney18@users.noreply.github.com>

--- public/javascript/bill.js
+++ public/javascript/bill.js
@@ -1,155 +1,110 @@
-const { json } = require("stream/consumers");
+/*
+WE ned to target the button and store it in a variable
+Once stored, we are are going to add an event listener to the button
+Once the button is clicked, then call the function
+*/
 
-let billTitle;
-let billText;
-let addBillBtn;
-let billList;
+const addBillBtn = document.getElementById('submitBill');
 
-if (window.location.pathname === '/bills') {
-    billTitle = document.querySelector('.bill-title');
-    billText = document.querySelector('.bill-textarea');
-    addBillBtn = document.querySelector('#add-bill');
-    billList = document.querySelectorAll('.list-container .list-group');
-}
 
-//activeBill is used to keep track of the note in the textarea
-let activeBill = {};
 
-const getBills = () =>
-fetch('/api/bills', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
+class BillClass {
+    constructor() {
+      this.bills = JSON.parse(localStorage.getItem('BILLS'));
+      if(!this.bills) {
+        this.bills = [
+          {bill: 'Room rent', isPaid: false},
+          {bill: 'Electricity bill', isPaid: true},
+          {bill: 'Internet bill', isPaid: false},
+        ];
+      }
 
-const saveBill = (bill) =>
-fetch('/api/bills', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(bill)
-});
-
-const deleteBill = (id) =>
-fetch('/api/bills/${id}', {
-    method: 'DELETE',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
-
-const renderActiveBill = () => {
-    if (activeBill.id) {
-        billTitle.setAttribute('readonly', true);
-        billText.setAttribute('readonly', true);
-        billTitle.value = activeBill.billTitle;
-        billText.value = activeBill.billText;
-    } else {
-        billTitle.removeAttribute('readonly');
-        billText.removedAttribute('readonly');
-        billTitle.value = '';
-        billText.value = '';
-    }
-};
-
-const handleBillSave = () => {
-    const newBill = {
-        title: billTitle.value,
-        text: billText.value,
-    };
-    saveBill(newBill).then(() => {
-        getAndRenderBills();
-        renderActiveBill();
-    });
-};
-
-//delete the clicked bill
-const handleBillDelete = (e) => {
-    //prevents the click listener for the list from being called when the button inside of it is clicked
-    e.stopPropagation();
-
-    const bill = e.target;
-    const billId = JSON.parse(note.parentElement.getAttribute('data-bill')).id;
-
-    if (activeBill.id === billId) {
-        activeBill = {};
+      this.loadBills();
+      this.addEventListeners();
     }
 
-    deleteBill(billId).then(() => {
-        getAndRenderBills();
-        renderActiveBill();
-    });
-};
-
-//sets the activeBill and displays it
-const handleBillView = (e) => {
-    e.preventDefault();
-    activeBill = JSON.parse(e.target.parentElement.getAttribute('data-bill'));
-    renderActiveBill();
-};
-
-//sets the activeBill to an empty object and allows the user to enter a new bill
-const handleNewBillView = (e) => {
-    activeBill = {};
-    renderActiveBill();
-};
-
-//render the list of bill titles
-const renderBillList = async (bills) => {
-    let jsonBills = await bills.json();
-    if (window.location.pathname === '/bills') {
-        billList.forEach((el) => (el.innerHTML = ''));
-    }
-
-    let billListItems = [];
-
-    //return HTML element with or without a delete button
-    const createLi = (text, delBtn = true) => {
-        const liEl = document.createElement('li');
-        liEl.classList.add('list-group-item');
-
-        const spanEl = document.createElement('span');
-        spanEl.classList.add('list-item-title');
-        spanEl.innerText = text;
-        spanEl.addEventListener('click', handleBillView);
-
-        liEl.append(spanEl);
-
-        if (delBtn) {
-            const delBtnEl = document.createElement('i');
-            delBtnEl.classList.add(
-                'fas',
-                'fa-trash-alt',
-                'float-right',
-                'text-danger',
-                'delete-bill'
-            );
-            delBtnEl.addEventListener('click', handleBillDelete);
-
-            liEl.append(delBtnEl)
+    addEventListeners() {
+      // Add bill
+	  
+      document.getElementById('addBill').addEventListener("keypress", event => {
+        if(event.keyCode === 13) {
+          this.addBill(event.target.value);
+          event.target.value = "";
         }
-        return liEl;
-    };
-    if (jsonBills.length === 0) {
-        billListItems.push(createLi('No saved Bills', false));
+      });
     }
 
-    jsonBills.forEach((bill) => {
-        const li = createLi(bill.title);
-        li.dataset.bill = JSON.stringify(bill);
-
-        billListItems.push(li);
-    });
-    if (window.location.pathname === '/bills') {
-        billListItems.forEach((bill) => billList[0].append(bill));
+    addBillClick() {
+      let target = document.getElementById('submitBill');
+      this.addBill(target.value);
+      target.value = ""
+      console.log(target.value)
     }
-};
 
-//gets bills from the db and renders them to page
-const getAndRenderBills = () => getBills().then(renderBillList);
+    addBill(bill) {
+      let newBill = {
+        bill,
+        isPaid: false,
+      };
+      console.log(newBill)
+      let parentDiv = document.getElementById('addBill').parentElement;
+      if(bill === '') {
+        parentDiv.classList.add('has-error');
+      } else {
+        parentDiv.classList.remove('has-error');
+        this.bills.push(newBill);
+        this.loadBills();
+      }
+    }
 
-if (window.location.pathname === '/bills') {
-    addBillBtn.addEventListener('click', handleBillSave);
+    toggleBillStatus(index) {
+      this.bills[index].isPaid = !this.bills[index].isPaid;
+      this.loadBills();
+      console.log(index)
+    }
+
+    deleteBill(event, billIndex) {
+      event.preventDefault();
+      this.bills.splice(billIndex, 1);
+      this.loadBills();
+    }
+
+    generateBillHtml(bill, index) {
+      return `
+        <li class="list-group-item checkbox">
+          <div class="row">
+            <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+              <label>
+                <input id="togglebillstatus" type="checkbox" onchange="toggleBillStatus(${index})" value="" 
+                  class="" ${bill.isPaid?'checked':''}>
+              </label>
+            </div>
+            <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 bill-text ${bill.isPaid?'Paid':''}">
+              ${bill.bill}
+            </div>
+            <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+              <a class="" href="/" onClick="deleteBill(event, ${index})">
+                <i id="deleteBill" data-id="${index}" class="delete-icon fa-solid fa-trash"></i>
+              </a>
+            </div>
+          </div>
+        </li>
+      `;
+    }
+
+    loadBills() {
+      localStorage.setItem('BILLS', JSON.stringify(this.bills));
+      let billsHtml = this.bills.reduce((html, bill, index) => html += this.generateBillHtml(bill, index), '');
+      document.getElementById('billList').innerHTML = billsHtml;
+    }
 }
+
+let toBill = new BillClass()
+
+// window.addEventListener("load", () => {
+//   toBill = new BillClass();
+// });
+
+addBillBtn.addEventListener('click', () => {
+    toBill.addBillClick()
+});

964040b1768a337809589c51fd8ecc358a921a13
samvrny <samvrny@gmail.com>
2022-09-06 13:34:55 -0500

    working on seaching a group by it's id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,9 +27,33 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbGroupData => {
-            const myGroup = dbGroupData.groups.map(melon => melon.get({ plain: true }));
-            const groups = dbGroupData.joined_group.map(group => group.get({ plain: true }));
+            let myGroup = dbGroupData.groups.map(melon => melon.get({ plain: true }));
+            let groups = dbGroupData.joined_group.map(group => group.get({ plain: true }));
             res.render('homepage', { groups, myGroup, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Group.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'group_name',
+            'address'
+        ]
+    })
+        .then(dbGroupData => {
+            // const myGroup = dbGroupData.groups.map(melon => melon.get({ plain: true }));
+            const cherry = dbGroupData.get({ plain: true });
+            console.log(cherry, 'BARBEQUE');
+            res.render('homepage', { cherry, loggedIn: true });
+            //res.json(cherry);
         })
         .catch(err => {
             console.log(err);

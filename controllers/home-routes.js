2e73f38b4304244d57519423d11300be83a71e05
samvrny <samvrny@gmail.com>
2022-09-05 21:28:01 -0500

    working on console logs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,8 +4,6 @@ const { Bill, Chore, Grocery, Group, Member, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    //console.log(req.session, "SESSION LOG HERE");
-    //console.log(req.session.user_id, "SESSION USER ID HERE");
     User.findOne({
         where: {
             id: req.session.user_id
@@ -29,11 +27,8 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbGroupData => {
-            //console.log(dbGroupData.joined_group);
-            //console.log(dbGroupData.groups);
             const myGroup = dbGroupData.groups.map(melon => melon.get({ plain: true }));
             const groups = dbGroupData.joined_group.map(group => group.get({ plain: true }));
-            // console.log(groups);
             res.render('homepage', { groups, myGroup, loggedIn: true });
         })
         .catch(err => {

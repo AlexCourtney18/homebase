ce4b20a84af8683bdec234514fda2ad9b8d60450
samvrny <105232555+samvrny@users.noreply.github.com>
2022-09-07 13:05:01 -0500

    Feature/sam (#77)
    * added my name to the README
    
    * created User model
    
    * build Bill model
    
    * build Group model
    
    * added Chore and Grocery models
    
    * create user-routes
    
    * add code to bill-routes
    
    * built some associations
    
    * add group-routes
    
    * add chore routes and modify bill routes
    
    * finished models and testing of current routes
    
    * commit before pulling
    
    * fix
    
    * finish grocery routes
    
    * remove test code
    
    * commit
    
    * tested grocery routes
    
    * commit before pulling
    
    * commit change to gitignore before pulling
    
    * move main.handlebars code into login
    
    * add dashboard and homepage routes
    
    * built server
    
    * remove commented out homepage/dashboard routes from controllers/index
    
    * create landing page and login route
    
    * stashing
    
    * manipulated routes
    
    * console.log
    
    * update/fix routes to enable login/redirection to homepage
    
    * commit
    
    * got logout working
    
    * commit changes before pulling
    
    * finish functionality to add grocery from dashboard
    
    * what is happening
    
    * why
    
    * made a member model
    
    * added a member class
    
    * got the join a group functionality working via a table join method
    
    * working on console logs
    
    * complete delete functionality for bills
    
    * minor changes
    
    * working on seaching a group by it's id
    
    * commit before pulling
    
    * fix minor bug with deleting grocery/bill
    
    * got the join group functionality totally completed
    
    * got the functionality for the add members to group totally working
    
    * minor changes
    
    * did some touch up styling
    
    * complete revert branch
    
    * add readme and minor changes to handlebars
    
    * minor changes to readme
    
    * fix issue preventing images from displaying on readme
    
    * minor changes to dashboard handlebars
    
    * added helpers
    
    * added some basic style
    
    * cleaned up project files
    
    * update screenshot of dashboard
    
    * added jaws capability
    
    Co-authored-by: Alex <lex.courtney18@gmail.com>

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DBNAME,process.env.DBUSER,process.env.DBPW, {
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.DBNAME,process.env.DBUSER,process.env.DBPW, {
     dialect: 'mysql',
-    //host: '127.0.0.1'
-    host: 'localhost'
+    host: 'localhost',
+    port: 3306
 });
 
 module.exports = sequelize;
